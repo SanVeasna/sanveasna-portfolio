4c03399a35b495b3e165ae24b0d54926e3f9b581
SanVeasna <sanveasna096@gmail.com>
2026-02-04 22:46:07 +0700

    update CV

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,7 +1,7 @@
 const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/San-Veasna.pdf";
-    link.download = "San-Veasna.pdf";
+    link.href = "/SanVeasna.pdf";
+    link.download = "SanVeasna.pdf";
     link.click();
   }
 
